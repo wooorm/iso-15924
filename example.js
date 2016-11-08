ee9edbc088d29f5ef23474ecc2b86cadaef7572d
Titus Wormer <tituswormer@gmail.com>
2016-11-08 16:51:53 +0100

    Remove superfluous files

--- example.js
+++
@@ -1,8 +0,0 @@
-// Dependencies:
-var iso15924 = require('./index.js');
-
-// Inspecting some values yields:
-console.log('js', require('util').inspect(iso15924.slice(0, 10)));
-
-// And the `length` computes to:
-console.log('js', String(iso15924.length));
